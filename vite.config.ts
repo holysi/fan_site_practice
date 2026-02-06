4b5e32700878a4b0e6f518b91eebab2d59cd7ea0
boris hsieh <holysi@hotmail.com>
2026-02-06 16:12:37 +0800

    chore: configure base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './', // GitHub Pages base
+    base: '/fan_site_practice/', // GitHub Pages base
 })
